359d40eb8598998aa784745d6eb7b0b6d6d35cdd
LuminousChestnut <59057924+LuminousChestnut@users.noreply.github.com>
2023-11-04 00:59:45 +0800

    replacer v0.2

---
+++ GitArchive/nlp/replacer.py
@@ -0,0 +1,13 @@
+def replacer(replaceString, replaceWordList):
+    """
+    2023-10-27 v0.1
+    replaceString(str) ：需要替换的字符串
+    replaceWordList(list) : 替换的列表
+    flagDisplay(int) ：是否显示旗帜（运行成功旗帜为0，运行失败旗帜为1）
+    """
+
+    try:
+        for word in replaceWordList:
+            replaceString = replaceString.replace(word, "")
+    except(ValueError, SyntaxError):
+        print("replacer 发生错误。")
